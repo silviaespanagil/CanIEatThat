422646069421506481e53fc79df4794eb99b1802
Silvia España Gil <81619759+silviaespanagil@users.noreply.github.com>
2024-09-17 11:13:42 +0200

    Create project

---
+++ CanIEatThat/CanIEatThat/CanIEatThatApp.swift
@@ -0,0 +1,17 @@
+//
+//  CanIEatThatApp.swift
+//  CanIEatThat
+//
+//  Created by Silvia España Gil on 17/9/24.
+//
+
+import SwiftUI
+
+@main
+struct CanIEatThatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
